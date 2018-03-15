da7b0dce866403ad5542d62fd42a52ff2c2f0204
liyong <55345249@qq.com>
2018-03-15 19:21:18 +0800

    测试6666

--- src/main/java/com/primeton/App.java
+++ src/main/java/com/primeton/App.java
@@ -9,8 +9,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @MapperScan("com.primeton.mapper")
 @ServletComponentScan(value = "com")
 public class App {
-
-//5555
+    //new
     public static void main(String args[]) {
         SpringApplication.run(App.class,args);
     }
