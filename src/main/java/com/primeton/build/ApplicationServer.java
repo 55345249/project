347cc6fe9547cb28a75db15b12bf76a1ac7217f7
sunxiaodan <sunxd@primeton.com>
2018-03-19 10:14:25 +0800

    component注解

--- src/main/java/com/primeton/build/ApplicationServer.java
+++ src/main/java/com/primeton/build/ApplicationServer.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.primeton.repository")
 @ServletComponentScan(value = "com")
+@ComponentScan("com")
 public class ApplicationServer {
     //11111
     public static void main(String args[]) {
