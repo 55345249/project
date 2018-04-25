a44ee6da7335738ba07da46efed0bed84d368a10
liyong <55345249@qq.com>
2018-04-25 14:25:29 +0800

    登陆后到跳到index

---
+++ src/main/java/com/primeton/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.primeton.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class LoginController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @GetMapping({ "/index" })
+    public String index(Model model) {
+
+        logger.info("--------index----------");
+
+        model.addAttribute("picUrl","/img/photo_s.jpg");
+
+        model.addAttribute("username", "admin");
+
+        return "index_v1";
+    }
+
+}
